aeb4f88798d5cbb687479c2cfcf6a35a01303c2d
NoraKri <106915799+NoraKri@users.noreply.github.com>
2022-10-31 15:00:39 +0100

    Initialized project

---
+++ src/test/java/no/hvl/dat108/Oblig4/Oblig4ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108.Oblig4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
